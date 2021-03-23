d2a55c813dd54b6acae9a95f1af3b30c9f01cdd4
Karina Song <ksysong@gmail.com>
2021-03-23 23:45:53 +0000

    add code so that it console logs how many characters are left

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,13 @@
-
-// ensures DOM is full loaded 
 $(document).ready(function() {
   console.log("is this working?")
 
   //add an event listener to the form field?
 
+  $("#tweet-text").on("input", function(event) {
+    const tweetLength = ($(this)).val().length;
+    let charsLeft = 140 - tweetLength;
+    console.log(charsLeft);
+    
+  })
 
 });
