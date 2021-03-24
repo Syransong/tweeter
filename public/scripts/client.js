e6f5ac4f98740d30a937d1a3c4cbdee1c5ff388e
Karina Song <ksysong@gmail.com>
2021-03-24 23:30:05 +0000

    add event listener on submit and ajax post request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(() => {
-  
-  // Array of tweet objects
-  const tweetData = [
-    {
+// Array of tweet objects
+const tweetData = [
+  {
+  "user": {
+    "name": "Kirby",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+  }, 
+  {
     "user": {
-      "name": "Kirby",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
     "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-    }, 
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+$(document).ready(() => {
   
   const renderTweets = function(tweets) {
     
@@ -70,6 +70,25 @@ $(document).ready(() => {
   }
 
   renderTweets(tweetData);
+
+  //event listener for submit 
+  $("form").on("submit", function(event) {
+    
+    event.preventDefault();
+
+    $.ajax({
+      url: "/tweets/",
+      method: 'POST',
+      data: $("form").serialize()
+    })
+    .done(()=>{
+      console.log("did it work?");
+    })
+    .fail((err) => {
+      console.log(err.message);
+    })
+  })
+
 });
 
 // console.log("$tweet", $tweet); //see what it looks like
