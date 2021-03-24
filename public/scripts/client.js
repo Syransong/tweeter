49ad8d4e5fcc6f5d24a6603fef047579d52ef77c
Karina Song <ksysong@gmail.com>
2021-03-24 23:01:48 +0000

    create renderTweets fcn to sort tweets based on time and append to the tweet-container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,7 +7,8 @@
 $(document).ready(() => {
   
   // tweet data object 
-  const tweetData = {
+  const tweetData = [
+    {
     "user": {
       "name": "Kirby",
       "avatars": "https://i.imgur.com/73hZDYK.png",
@@ -17,35 +18,32 @@ $(document).ready(() => {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
     "created_at": 1461116232227
-  }
-
-  //convert long date to regular date 
-  // const tweetDate = new Date(tweetData.created_at);
-  // console.log(tweetDate);
-  // tweetDate.toDateString();
-  // console.log(tweetDate);
-  // const datePosted = Date.now() - tweetDate;
-  // console.log(datePosted.toDateString());
-
-  // const tweetDate = new Date(tweetData.created_at);
-  // const presentDate = new Date.now();
-
-// construct new tweet using jQuery
-
-const createTweetElement = function(tweetObj) {
+    }, 
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+const createTweetElement = function(tweet) {
   
-  const newTweet = $(
+  let $newTweet = $(
     `<article class="tweet">
       <header>
-        <img class="profile-pic" src=${tweetObj.user.avatars}">
+        <img class="profile-pic" src=${tweet.user.avatars}">
         <div class="profile-info">
-         <p>${tweetObj.user.name}</p>
-         <p class="username">${tweetData.user.handle}</p>
+         <p>${tweet.user.name}</p>
+         <p class="username">${tweet.user.handle}</p>
         </div>
       </header>
-      <p>${tweetObj.content.text}</p>
+      <p>${tweet.content.text}</p>
       <footer>
-       <p>${moment(tweetObj.created_at).fromNow()}</p>
+       <p>${moment(tweet.created_at).fromNow()}</p>
        <div class="icons">
         <i class="fas fa-flag"></i>
         <i class="fas fa-retweet"></i>
@@ -54,12 +52,26 @@ const createTweetElement = function(tweetObj) {
       </footer>
     </article>`
   );
-  return newTweet;
+  return $newTweet;
 }
 
-  const $tweet = createTweetElement(tweetData);
+  // const $tweet = createTweetElement(tweetData);
   
-  // console.log("$tweet", $tweet); //see what it looks like
-  $(".posted-tweets").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes 
+  
+  const renderTweets = function(tweets) {
+    
+    //sort tweets from latest to older
+    let sortedTweets = tweets.sort((a, b) => b.created_at - a.created_at);
+    
+    //loops through sorted tweets and appends them to the container
+    //calls createTweetElement for each tweet 
+    //takes return value and appends it to the tweets container 
+    for (let tweet of sortedTweets) {
+      $(".posted-tweets").append(createTweetElement(tweet))
+    }    
+  }
+  renderTweets(tweetData);
+});
 
-})
+// console.log("$tweet", $tweet); //see what it looks like
+// $(".posted-tweets").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes 
