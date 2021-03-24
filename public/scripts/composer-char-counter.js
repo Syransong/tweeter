2e18ccf0e1c50b228aa0356fcd036d0f154b22f6
Karina Song <ksysong@gmail.com>
2021-03-24 00:16:56 +0000

    eslint code and format

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
-  console.log("is this working?")
 
-  //add an event listener to the form field?
-
-  $("#tweet-text").on("input", function(event) {
+  $("#tweet-text").on("input", function() {
     const tweetLength = ($(this)).val().length;
     const counter = $(this).closest("form").find(".counter");
     const maxLength = 140;
-
+    
+    counter.text(maxLength - tweetLength);
+   
     if (tweetLength > maxLength) {
       counter.addClass("char-exceeded");
     }
-    
-    counter.text(maxLength - tweetLength);
-  })
+
+  });
 });
